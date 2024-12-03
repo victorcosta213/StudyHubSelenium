b145ac7ed5080825bfb02e40f7eb7a454b6457e7
victor.costa213 <132701394+victorcosta213@users.noreply.github.com>
2024-12-03 10:48:14 -0300

    novo caso de teste

--- StudyHubSelenium/testando/demo/src/test/java/ArtigosTest.java
+++ StudyHubSelenium/testando/demo/src/test/java/ArtigosTest.java
@@ -6,126 +6,79 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNot.not;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.Keys;
+
+import java.time.Duration;
 import java.util.*;
-import java.net.MalformedURLException;
-import java.net.URL;
+
 public class ArtigosTest {
   private WebDriver driver;
-  private Map<String, Object> vars;
-  JavascriptExecutor js;
+  private JavascriptExecutor js;
+
   @Before
   public void setUp() {
     driver = new ChromeDriver();
     js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
   }
+
   @After
   public void tearDown() {
     driver.quit();
   }
+
   @Test
-  public void artigos() {
+  public void artigos() throws InterruptedException {
     driver.get("https://sth-front-dev.vercel.app/student");
     driver.manage().window().setSize(new Dimension(1382, 736));
-    driver.findElement(By.cssSelector(".MuiListItem-root:nth-child(3) > .MuiTypography-root")).click();
-    driver.findElement(By.id(":r1:")).click();
-    driver.findElement(By.id(":r1:")).click();
-    {
-      WebElement element = driver.findElement(By.cssSelector(".MuiSelect-select"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element).clickAndHold().perform();
-    }
-    {
-      WebElement element = driver.findElement(By.cssSelector(".MuiBackdrop-root"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element).release().perform();
-    }
-    driver.findElement(By.cssSelector("body")).click();
-    driver.findElement(By.cssSelector(".MuiButtonBase-root:nth-child(2)")).click();
-    driver.findElement(By.id(":r1:")).click();
-    driver.findElement(By.id(":r1:")).sendKeys("clovis");
-    driver.findElement(By.id(":r1:")).click();
-    driver.findElement(By.id(":r1:")).click();
-    {
-      WebElement element = driver.findElement(By.id(":r1:"));
-      Actions builder = new Actions(driver);
-      builder.doubleClick(element).perform();
-    }
-    driver.findElement(By.id(":r1:")).sendKeys("clovis");
-    {
-      WebElement element = driver.findElement(By.cssSelector(".MuiSelect-select"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element).clickAndHold().perform();
-    }
-    {
-      WebElement element = driver.findElement(By.cssSelector(".MuiBackdrop-root"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element).release().perform();
-    }
-    driver.findElement(By.cssSelector("body")).click();
-    driver.findElement(By.cssSelector(".MuiButtonBase-root:nth-child(1)")).click();
-    driver.findElement(By.id(":r1:")).click();
-    driver.findElement(By.id(":r1:")).sendKeys("css");
-    {
-      WebElement element = driver.findElement(By.cssSelector(".MuiSelect-select"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element).clickAndHold().perform();
-    }
-    {
-      WebElement element = driver.findElement(By.cssSelector(".MuiBackdrop-root"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element).release().perform();
-    }
-    driver.findElement(By.cssSelector("body")).click();
-    driver.findElement(By.cssSelector(".MuiBackdrop-root")).click();
-    driver.findElement(By.id(":r1:")).click();
-    {
-      WebElement element = driver.findElement(By.id(":r1:"));
-      Actions builder = new Actions(driver);
-      builder.doubleClick(element).perform();
-    }
-    driver.findElement(By.id(":r1:")).click();
-    driver.findElement(By.id(":r1:")).sendKeys("react");
-    driver.findElement(By.id(":r1:")).sendKeys("react");
-    {
-      WebElement element = driver.findElement(By.cssSelector(".MuiSelect-select"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element).clickAndHold().perform();
-    }
-    {
-      WebElement element = driver.findElement(By.cssSelector(".MuiBackdrop-root"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element).release().perform();
-    }
-    driver.findElement(By.cssSelector("body")).click();
-    driver.findElement(By.cssSelector(".MuiButtonBase-root:nth-child(2)")).click();
-    driver.findElement(By.id(":r1:")).click();
-    driver.findElement(By.id(":r1:")).sendKeys("anna");
-    driver.findElement(By.id(":r1:")).sendKeys("anna");
-    {
-      WebElement element = driver.findElement(By.cssSelector(".MuiSelect-select"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element).clickAndHold().perform();
-    }
-    {
-      WebElement element = driver.findElement(By.cssSelector(".MuiBackdrop-root"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element).release().perform();
-    }
-    driver.findElement(By.cssSelector("body")).click();
-    driver.findElement(By.cssSelector(".MuiButtonBase-root:nth-child(1)")).click();
-  
+
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    
+    WebElement menuItem = driver.findElement(By.cssSelector(".MuiListItem-root:nth-child(3) > .MuiTypography-root"));
+    menuItem.click();
+    Thread.sleep(1000); 
+
+    WebElement input = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(":r1:")));
+
+ 
+    clearAndType(input, "clovis");
+    Thread.sleep(1000);
+    clearAndType(input, "css");
+    Thread.sleep(1000);
+    clearAndType(input, "react");
+    Thread.sleep(1000);
+    clearAndType(input, "anna");
+    Thread.sleep(1000);
+
+    
+    simulateDropdownInteraction(wait);
+
+   
+  }
+
+  private void clearAndType(WebElement element, String text) throws InterruptedException {
+    element.click();
+    element.sendKeys(Keys.CONTROL, "a");
+    element.sendKeys(Keys.DELETE);      
+    Thread.sleep(500);                 
+    element.sendKeys(text);             
+    Thread.sleep(500);                 
+  }
+
+  private void simulateDropdownInteraction(WebDriverWait wait) throws InterruptedException {
+    WebElement dropdown = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".MuiSelect-select")));
+    dropdown.click();
+    Thread.sleep(1000); 
+
+    WebElement backdrop = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".MuiBackdrop-root")));
+    backdrop.click();
+    Thread.sleep(1000); 
   }
 }
--- StudyHubSelenium/testando/demo/src/test/java/TesteLoginTest.java
+++ StudyHubSelenium/testando/demo/src/test/java/TesteLoginTest.java
@@ -2,28 +2,23 @@ import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsNot.not;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.JavascriptExecutor;
-import java.util.*;
+import java.time.Duration;
 
 public class TesteLoginTest {
     private WebDriver driver;
-    private Map<String, Object> vars;
-    JavascriptExecutor js;
 
     @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe"); 
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         driver = new ChromeDriver();
-        js = (JavascriptExecutor) driver;
-        vars = new HashMap<String, Object>();
+        driver.manage().window().setSize(new Dimension(1382, 736));
     }
 
     @After
@@ -32,20 +27,42 @@ public class TesteLoginTest {
     }
 
     @Test
-    public void testeLogin() {
+    public void testeLoginComInformacoesValidas() {
         driver.get("https://sth-front-dev.vercel.app/login");
-        driver.manage().window().setSize(new Dimension(1382, 736));
-        
-        driver.findElement(By.id(":r0:")).sendKeys("victor@test.com");
-        
-        
-        WebElement element = driver.findElement(By.cssSelector(".MuiButton-root"));
-        Actions builder = new Actions(driver);
-        builder.moveToElement(element).perform();
 
-      
+       
+        driver.findElement(By.id(":r0:")).sendKeys("victor@test.com");
         driver.findElement(By.id(":r1:")).sendKeys("1234567");
 
+        
         driver.findElement(By.cssSelector(".MuiButton-root")).click();
+
+        
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.urlContains("https://sth-front-dev.vercel.app"));
+
+       
+        String urlAtual = driver.getCurrentUrl();
+        assertTrue("Erro: não foi redirecionado para a página principal.", urlAtual.contains("https://sth-front-dev.vercel.app"));
+    }
+
+    @Test
+    public void testeLoginComInformacoesInvalidas() {
+        driver.get("https://sth-front-dev.vercel.app/login");
+
+       
+        driver.findElement(By.id(":r0:")).sendKeys("usuario@invalido.com");
+        driver.findElement(By.id(":r1:")).sendKeys("senha_incorreta");
+
+       
+        driver.findElement(By.cssSelector(".MuiButton-root")).click();
+
+        /*// Verificar mensagem de erro na tela
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement mensagemErro = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mensagem-erro")));
+
+       
+        assertNotNull("Erro: mensagem de erro não encontrada.", mensagemErro);
+        assertEquals("Usuário ou senha inválidos", mensagemErro.getText());*/
     }
 }
